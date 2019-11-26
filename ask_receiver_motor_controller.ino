860a0f99fc64e7186a0e087658444cba66e58c4d
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2019-11-26 00:09:20 -0600

    ask rf rx code

---
+++ ask_receiver_motor_controller.ino
@@ -0,0 +1,42 @@
+/* 
+ *  This program was creatd, ....'''
+ *  
+ *  
+ *    This program was created by Tyler Adam Martinez
+ */
+
+#include <RH_ASK.h>
+#ifdef RH_HAVE_HARDWARE_SPI
+#include <SPI.h> // Not actually used but needed to compile
+#endif
+
+RH_ASK driver(2000, 0, 1, 2);
+// RH_ASK driver(2000, 4, 5, 0); 
+// RH_ASK driver(2000, 3, 4, 0); 
+
+void setup()
+{
+#ifdef RH_HAVE_SERIAL
+    Serial.begin(9600);	  // Debugging only
+#endif
+    if (!driver.init())
+#ifdef RH_HAVE_SERIAL
+         Serial.println("init failed");
+#else
+	;
+#endif
+}
+
+void loop()
+{
+    uint8_t buf[RH_ASK_MAX_MESSAGE_LEN];
+    uint8_t buflen = sizeof(buf);
+
+    if (driver.recv(buf, &buflen)) // Non-blocking
+    {
+	int i;
+
+	// Message with a good checksum received, dump it.
+	driver.printBuffer("Got:", buf, buflen);
+    }
+}
